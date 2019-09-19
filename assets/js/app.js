3c692c940b323b559f61c7847dc706a422f132cb
erika gonzalez <gonzalezerika75951@gmail.com>
2019-09-19 00:12:05 -0500

    added a forloop to have my buttons become active and present on the screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,9 @@ var topics = [
 	"seal",
 	"flamingo",
 ];
+// added my forloop to add new button each time someone searches an animal!
+for(var i = 0; i < topics.length; i++) {
+	var button = $("<button>").text(topics[i]);
+	button.attr("data-animal", topics[i]);
+	button.addClass("animal-button");
+	$("#button-group").append(button);
