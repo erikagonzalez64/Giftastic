49fe206dae2bec05acdcd4534f76a6f89b155d7b
erika gonzalez <gonzalezerika75951@gmail.com>
2019-09-19 00:15:15 -0500

    added an if statement and buttons for each animal that is new searched

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,4 +9,22 @@ for(var i = 0; i < topics.length; i++) {
 	var button = $("<button>").text(topics[i]);
 	button.attr("data-animal", topics[i]);
 	button.addClass("animal-button");
-	$("#button-group").append(button);
+    $("#button-group").append(button);
+}
+//added an if statement. Also added buttons and classes to be able to add new animal each type it is search with new button
+$("#add-animal-button").on("click", function(e) {
+	e.preventDefault();
+	var alreadyExist = false;
+	if(topics.indexOf($("#new-animal-input").val()) !== -1) {
+		alreadyExist = true;
+	}
+	if($("#new-animal-input").val() !== "" && alreadyExist === false) {
+		var newAnimal = $("#new-animal-input").val().toLowerCase();
+		topics.push(newAnimal);
+		var button = $("<button>").text(newAnimal);
+		button.attr("data-animal", newAnimal);
+		button.addClass("animal-button");
+		$("#button-group").append(button);
+	}
+	$("#new-animal-input").val("");
+});
